a3f5e9cb8e565eb44c4b6b24d390564ce948d1ed
1285963544@qq.com <1285963544@qq.com>
2018-01-02 15:30:23 +0800

    添加项目文件

---
+++ src/com/shsxt/app/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.shsxt.app;
+/**
+ * 测试代码
+ * @author Mr.YongGan.Zhang
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
